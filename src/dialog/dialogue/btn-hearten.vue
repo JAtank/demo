461ffa5ca50a1524cc1bcbf2034ead7e939505eb
zhoutengteng <zhoutengteng@tbkt.cn>
2019-06-13 18:52:06 +0800

    fix 贝塞尔

--- src/dialog/dialogue/btn-hearten.vue
+++ src/dialog/dialogue/btn-hearten.vue
@@ -6,7 +6,7 @@
     <svg version="1.1">
       <circle cx="50%" cy="100%" r="20" fill="blue">
         <animate attributeName="r" from="20" to="100" dur="2" fill="freeze"></animate>
-        <animatemotion path="m50% 100%  c30% 90% 40% 60% 35% 50% s45% 45% 30% 30%" dur="2"></animatemotion>
+        <animateMotion path="m50% 100%  c30% 90% 40% 60% 35% 50% s45% 45% 30% 30%" begin="0" dur="2"></animateMotion>
       </circle>
     </svg>
   </div>
